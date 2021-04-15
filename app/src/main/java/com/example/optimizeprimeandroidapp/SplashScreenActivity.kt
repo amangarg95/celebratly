5122f1d9cc7ce465e033415a6bba53828cd31940
Aman Garg <aman.g@kiprosh.com>
2021-04-15 19:50:05 +0530

    Basic splash screen added.

---
+++ app/src/main/java/com/example/optimizeprimeandroidapp/SplashScreenActivity.kt
@@ -0,0 +1,25 @@
+package com.example.optimizeprimeandroidapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        Handler().postDelayed({
+            val intent = Intent(
+                this@SplashScreenActivity,
+                MainActivity::class.java
+            )
+            startActivity(intent)
+            finish()
+        }, SPLASH_SCREEN_TIME_OUT.toLong())
+    }
+
+    companion object {
+        private const val SPLASH_SCREEN_TIME_OUT = 2000
+    }
+}
