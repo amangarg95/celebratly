c07841ee097456ba503a0a18d5b30fed5177e516
Aman Shahu <aman.shahu@kiprosh.com>
2021-04-25 17:21:04 +0530

    WIP

--- app/src/main/java/com/kiprosh/optimizeprime/view/activity/SignInActivity.kt
+++ app/src/main/java/com/kiprosh/optimizeprime/view/activity/SignInActivity.kt
@@ -1,4 +1,4 @@
-package com.kiprosh.optimizeprime
+package com.kiprosh.optimizeprime.view.activity
 
 import android.content.Intent
 import android.os.Bundle
@@ -7,31 +7,35 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.messaging.ktx.messaging
-import com.kiprosh.optimizeprime.databinding.ActivitySingInBinding
+import com.kiprosh.optimizeprime.R
+import com.kiprosh.optimizeprime.databinding.ActivityLoginBinding
 
 class SignInActivity : AppCompatActivity(), View.OnClickListener {
 
-    lateinit var binding: ActivitySingInBinding
+    lateinit var binding: ActivityLoginBinding
     private lateinit var googleSignInClient: GoogleSignInClient
     private lateinit var auth: FirebaseAuth
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_sing_in)
-        binding.btnGoogleSignIn.setOnClickListener(this)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+        binding.googleButton.setOnClickListener(this)
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .setHostedDomain(getString(R.string.sign_in_domain))
-                .requestIdToken(getString(R.string.auth_api_key))
-                .requestEmail()
-                .build()
+            .setHostedDomain(getString(R.string.sign_in_domain))
+            .requestIdToken(getString(R.string.auth_api_key))
+            .requestEmail()
+            .build()
 
         googleSignInClient = GoogleSignIn.getClient(this, gso)
         googleSignInClient.signOut()
@@ -55,21 +59,27 @@ class SignInActivity : AppCompatActivity(), View.OnClickListener {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == RC_SIGN_IN) {
             val task = GoogleSignIn.getSignedInAccountFromIntent(data)
-            try {
-                // Google Sign In was successful, authenticate with Firebase
-                val account = task.getResult(ApiException::class.java)!!
-                Log.d(
-                    TAG,
-                    "firebaseAuthWithGoogle:" + account.id + account.email + account.givenName
-                )
-                Log.d(TAG, "Token" + account.idToken)
-                Log.d("Image", account.photoUrl.toString() + "")
+            if (task.isSuccessful) {
+                try {
+                    val account = task.getResult(ApiException::class.java)!!
+                    Log.d(
+                        TAG,
+                        "firebaseAuthWithGoogle:" + account.id + account.email + account.givenName
+                    )
+                    FirebaseMessaging.getInstance().token.addOnCompleteListener { tokenTask ->
+                        {
+                            if (tokenTask.isSuccessful) {
+                                tokenTask.result
+                            }
+                        }
 
+                    }
+                } catch (e: ApiException) {
+                    // Google Sign In failed, update UI appropriately
+                    Log.w(TAG, "Google sign in failed", e)
+                }
+            } else {
 
-                //firebaseAuthWithGoogle(account.idToken!!)
-            } catch (e: ApiException) {
-                // Google Sign In failed, update UI appropriately
-                Log.w(TAG, "Google sign in failed", e)
             }
         }
     }
@@ -77,5 +87,10 @@ class SignInActivity : AppCompatActivity(), View.OnClickListener {
     companion object {
         private const val TAG = "GoogleActivity"
         private const val RC_SIGN_IN = 9001
+    }
+
+    private fun getAuthenticationToken(googleTask: Task<GoogleSignInAccount>) {
+
+
     }
 }
