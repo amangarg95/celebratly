cc921534b2ee6eb442430dec22334348680aba0f
Aman Shahu <aman.shahu@kiprosh.com>
2021-04-30 04:43:29 +0530

    display image in the notification

--- app/src/main/java/com/kiprosh/optimizeprime/services/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/kiprosh/optimizeprime/services/MyFirebaseMessagingService.kt
@@ -6,6 +6,7 @@ import android.app.PendingIntent
 import android.content.Intent
 import android.os.Build
 import androidx.core.app.NotificationCompat
+import com.bumptech.glide.Glide
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import com.kiprosh.optimizeprime.R
@@ -19,7 +20,6 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
         val intent = Intent(this, MainActivity::class.java)
-        //intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_BROUGHT_TO_FRONT or Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_NO_ANIMATION)
         intent.addFlags(Intent.FLAG_ACTIVITY_BROUGHT_TO_FRONT)
         val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
         val builder: NotificationCompat.Builder = NotificationCompat.Builder(this, defaultChannel)
@@ -28,6 +28,17 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
             .setContentText(message.notification!!.body).setAutoCancel(true)
             .setContentIntent(pendingIntent)
         val manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
+        if (message.notification!!.imageUrl != null) {
+            val bigPicture = NotificationCompat.BigPictureStyle()
+            val futureTarget = Glide.with(this)
+                .asBitmap()
+                .load(message.notification!!.imageUrl)
+                .submit()
+            val bitmap = futureTarget.get()
+            builder.setStyle(bigPicture.bigPicture(bitmap))
+        }
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val channel = NotificationChannel(
                 defaultChannel,
